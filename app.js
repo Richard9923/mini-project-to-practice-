a36fe019b36ef22e9567fbe13f4723ec6061b91b
Richard <programmingislife23@gmail.com>
2024-04-07 10:41:05 -0300

    realizada a criacao das funcoes base do jogo

---
+++ app.js
@@ -0,0 +1,33 @@
+function playerChoice() {
+  choice = prompt("Escola entre pedra papel tesoura");
+  return choice;
+}
+
+function computerChoice() {
+  computerOptions = ["pedra", "papel", "tesoura"];
+  randomNumber = Math.floor(Math.random() * computerOptions.length);
+  console.log(randomNumber);
+}
+
+function game(player, computer) {
+  if (player == computer) {
+    console.log(
+      `Foi empate, ambos fizeram a mesma escolha. Jogador:${player}. Computador: ${computer}.`
+    );
+    return "empate";
+  } else if (player == "pedra" && computer == "tesoura") {
+    console.log(`O jogador venceu. Jogador: ${player}. Computer: ${computer}.`);
+    return "player venceu";
+  } else if (player == "papel" && computer == "pedra") {
+    console.log(`O jogador venceu. Jogador: ${player}. Computer: ${computer}.`);
+    return "player venceu";
+  } else if (player == "tesoura" && computer == "papel") {
+    console.log(`O jogador venceu. Jogador: ${player}. Computer: ${computer}.`);
+    return "player venceu";
+  } else {
+    console.log(
+      `O Computador venceu. Computer: ${computer}. Player: ${player}.`
+    );
+    return "player venceu";
+  }
+}
